e083f626306a322a526a379903e51e5efd2359ee
Vaishnavi K <66245171+Vaishu2001@users.noreply.github.com>
2020-10-05 23:35:45 +0530

    Add files via upload

---
+++ Day 12/assignment.js
@@ -0,0 +1,10 @@
+const any=document.body;
+any.style.backgroundColor = "black";
+any.style.color='white';
+
+const title = document.querySelector('title');
+title.textContent = "Assignment - Solved!";
+const title1 = document.head.querySelector('title');
+title1.textContent="Assignment - Solved!";
+const any1 = any.getElementsByTagName("h1");
+any1[0].textContent = "Assignment - Solved!";
